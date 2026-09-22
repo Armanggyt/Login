b05dcaa16acc8b62a1d25d179b81061dfad63b67
Arman <armanggyt@gmail.com>
2026-09-23 01:10:31 +0930

    Database

--- src/Controller/RegSubmit.php
+++ src/Controller/RegSubmit.php
@@ -3,6 +3,7 @@ namespace Login\Controller;
 
 class RegSubmit {
     public function html() {}
+    
     public function submit() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['username'] ?? '';
@@ -19,11 +20,21 @@ class RegSubmit {
                 exit;
             }
 
-            $regObject = new \Login\Entity\User();
-            $regObject->setCredentials($username, $password);
-            print( "Hello, " . $username . "!" );
-            print("<br>");
-            print("Your password is: " . $password);
+          $l  = mysqli_connect('db','user', 'user');
+            if (!mysqli_select_db($l, 'default')) {
+            die('Cant select db');
+}
+
+            $sql = "CREATE TABLE IF NOT EXISTS users (
+             id INT AUTO_INCREMENT PRIMARY KEY,
+             name VARCHAR(50) NOT NULL,
+             password VARCHAR(255) NOT NULL
+)";
+            mysqli_query($l, $sql);
+            
+            $insert_sql = "INSERT INTO users (name, password) VALUES ('$username', '$password')";
+            mysqli_query($l, $insert_sql);
+            mysqli_close($l);
         }
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,15 +1,18 @@
 <?php
 
-namespace Login\Entity; 
+namespace Login\Entity;
 
-class User {
+class User
+{
     protected $uid;
     protected $email;
     protected $pass;
     protected $created;
     protected $data;
-   public function setCredentials($username, $password) {
+
+    public function setCredentials($username, $password)
+    {
         $this->uid = $username;
         $this->pass = $password;
-   }
-};
+    }
+}
